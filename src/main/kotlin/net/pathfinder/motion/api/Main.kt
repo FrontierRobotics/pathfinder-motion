69df9f25f7231c308f223706fb7c700e73460c68
Andy Condon <andrew.condon@echostar.com>
2016-03-13 22:53:47 -0600

    Working in stuff from an example.

--- src/main/kotlin/net/pathfinder/motion/api/Main.kt
+++ src/main/kotlin/net/pathfinder/motion/api/Main.kt
@@ -1,5 +1,10 @@
 package net.pathfinder.motion.api
 
+import com.pi4j.io.i2c.I2CBus
+import com.pi4j.io.i2c.I2CFactory
+
 fun main(args: Array<String>) {
     println("Motion!")
+
+    val bus = I2CFactory.getInstance(I2CBus.BUS_1)
 }
